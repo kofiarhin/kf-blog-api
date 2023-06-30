29673dd00865e6eab3aad8dad7fb3a7d158e0e07
kofi arhin <kofiarhin69@gmail.com>
2023-06-30 15:21:12 +0100

    updaste to dotenv file

--- db/config.js
+++ db/config.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 const connectDB = () => {
-    const mongoUrl = process.env.MONGOURL;
+    const mongoUrl = process.env.MONGO_URL;
     mongoose.connect(mongoUrl).then( () => console.log("connected to database"))
 }
 
